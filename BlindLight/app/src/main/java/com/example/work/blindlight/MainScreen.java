0aa602c3843e990729b3ff0d52967fa506cbbd34
klavdijS <klavdij.starman1@gmail.com>
2016-05-14 18:16:21 +0200

    notificationi plus view dela

--- BlindLight/app/src/main/java/com/example/work/blindlight/MainScreen.java
+++ BlindLight/app/src/main/java/com/example/work/blindlight/MainScreen.java
@@ -1,27 +1,22 @@
 package com.example.work.blindlight;
 
-import android.support.v7.app.AppCompatActivity;
-
-import android.os.Bundle;
 
 import android.app.Activity;
-
 import android.app.AlertDialog;
-
-import android.app.AlertDialog.Builder;
-
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Context;
-
 import android.content.DialogInterface;
-
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
 import android.view.Menu;
-
 import android.view.View;
-
 import android.view.View.OnClickListener;
-
 import android.widget.Button;
-
 
 
 public class MainScreen extends Activity  {
@@ -30,7 +25,31 @@ public class MainScreen extends Activity  {
 
     Context context = this;
 
+    private Button testButton;
 
+
+    public void createNotification(View view) {
+
+        //Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        Notification n = new Notification.Builder(this)
+                .setContentTitle("New mail from " + "test@gmail.com")
+                .setContentText("Subject")
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setPriority(Notification.PRIORITY_MAX)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Ringtone r = RingtoneManager.getRingtone(context.getApplicationContext(), notification);
+        r.play();
+
+        notificationManager.notify(0, n);
+        Log.v("v","Test");
+
+    }
 
     @Override
 
@@ -41,11 +60,18 @@ public class MainScreen extends Activity  {
         setContentView(R.layout.activity_main_screen);
 
 
-
+        testButton = (Button)findViewById(R.id.button2);
         //button's reference
 
         button = (Button)findViewById(R.id.button);
 
+        testButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNotification(v);
+            }
+
+        });
 
 
         //setting button's click and implementing the onClick method
