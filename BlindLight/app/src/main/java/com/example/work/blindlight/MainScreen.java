7c3b4db0157e0521df9afbdf39491927f0cc0ebf
Miha Mohorčič <mohimm@gmail.com>
2016-05-14 15:21:44 +0200

    basic android project

---
+++ BlindLight/app/src/main/java/com/example/work/blindlight/MainScreen.java
@@ -0,0 +1,13 @@
+package com.example.work.blindlight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
